5ef3daf815e5d18ea952a6771bb22ff183f051d5
tofayeLL <ahmedtofayel132@gmail.com>
2024-07-17 23:11:57 +0600

    use POst method store users info in database with password hashing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
+const bcrypt = require('bcryptjs');
 const port = process.env.PORT || 5000;
 
 
@@ -11,18 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 
-// QuickCash
-// VgjQUMqcfyBb13U4
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8mgufzz.mongodb.net/?appName=Cluster0`;
 
-
-
-
-
-
-const uri = "mongodb+srv://QuickCash:VgjQUMqcfyBb13U4@cluster0.8mgufzz.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -37,6 +31,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    // user collection
+    const userCollection = client.db("QuickCash").collection("users");
+
+
+
+
+
+    // Post from signin users
+    app.post('/users', async (req, res) => {
+      // for hashing pin or password use bcrypt.js
+      const salt =  bcrypt.genSaltSync(10);
+      const secPassword = bcrypt.hashSync(req.body.password, salt);
+      req.body.password = secPassword;
+
+      const user = req.body;
+      
+      const query = { email: user.email }
+      const alreadyExist = await userCollection.findOne(query);
+      if (alreadyExist) {
+        return res.send({ message: 'User Already Exist', insertedId: null })
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -52,10 +85,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('QuickCash server is running...');
+  res.send('QuickCash server is running...');
 })
 
 
 app.listen(port, () => {
-    console.log(`QuickCash server is running at port:${port}`);
+  console.log(`QuickCash server is running at port:${port}`);
 })
